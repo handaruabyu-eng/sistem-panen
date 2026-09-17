a5d8f2426bf41fb145dbd2f3f759d0d085cf8623
handaruabyu-eng <handaruabyu@gmail.com>
2026-09-17 14:21:38 +0700

    Menambahkan program penghitung total hasil panen

---
+++ panen.py
@@ -0,0 +1,12 @@
+def hitung_total(jumlah, harga):
+    return jumlah * harga
+
+
+jumlah = 100
+harga = 5000
+
+total = hitung_total(jumlah, harga)
+
+print("Jumlah hasil panen:", jumlah, "kg")
+print("Harga per kg: Rp", harga)
+print("Total hasil panen: Rp", total)
